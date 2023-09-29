12fc56c9dfea1696fd4e7c71b0f1017e611dfb2a
yuuka83 <s2210017@jaist.ac.jp>
2023-09-29 18:36:08 +0900

    Characterクラスの追加

---
+++ Assets/Character.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public bool isPlayer;// ‚¢‚ç‚È‚¢‚©‚à
+
+    public bool isDying;// •m€ó‘Ô‚©
+
+    public bool isFainted;//‹Câó‘Ô‚©
+}
