04a0defea5b028d4e31981d2fe3b78a08b1f489e
belltree1022 <s2310078@jaist.ac.jp>
2023-11-28 19:50:02 +0900

    ソナーの演出、アイテム周りの実装ができた

--- Assets/Scripts/ActivateSonar.cs
+++ Assets/Scripts/ActivateSonar.cs
@@ -1,32 +1,40 @@
 using UnityEngine;
+using DG.Tweening; //DOTWEENをインポート
 
-
-// シフトボタンを押したらソナーが使えるスクリプト
 public class ActivateSonar : MonoBehaviour
 {
-    public GameObject targetGameObject; // アクティブにする対象のゲームオブジェクト
+    public GameObject targetGameObject;
+    private Tween scaleTween; //tween型の変数scaletween
+    
 
     void Update()
     {
         // 左Shiftか右Shiftのいずれかが押されたとき
         if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
         {
-            if (targetGameObject != null) // 対象のゲームオブジェクトが設定されているかチェック
+            if (targetGameObject != null)
             {
                 targetGameObject.SetActive(true); // ゲームオブジェクトをアクティブにする
-            }
-            else
-            {
+                scaleTween = targetGameObject.transform.DOScale(new Vector3(2f, 2f, 2f), 3f); // スケールを0から2にアニメーションする
             }
         }
+
+        // 左Shiftか右Shiftのいずれかを離したとき
         if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
         {
-            if (targetGameObject != null) // 対象のゲームオブジェクトが設定されているかチェック
+            if (targetGameObject != null)
             {
-                targetGameObject.SetActive(false); // ゲームオブジェクトを非アクティブにする
-            }
-            else
-            {
+                if (scaleTween != null && scaleTween.IsActive())
+                {
+                    scaleTween.Kill(); // 現在のスケールアニメーションを停止
+                }
+
+                // スケールを徐々に0にアニメーションする
+                // DOScale(Vector3.zero, 2f) 2秒後に元の大きさに戻す。
+                // OnComplete コールバック関数　ある関数が実行された後に呼び出される関数を無視する
+                targetGameObject.transform.DOScale(Vector3.zero, 2f).OnComplete(() => {
+                    targetGameObject.SetActive(false); // アニメーション完了後、ゲームオブジェクトを非アクティブにする
+                });
             }
         }
     }
