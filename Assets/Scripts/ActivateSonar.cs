ef7dcfb8e56adcb069ce894ac335f481d125f299
井上悠香 <inoueyuka@d175-214.jaist.ac.jp>
2023-11-30 20:22:07 +0900

    微修正

--- Assets/Scripts/ActivateSonar.cs
+++ Assets/Scripts/ActivateSonar.cs
@@ -3,27 +3,36 @@ using DG.Tweening; //DOTWEENをインポート
 
 public class ActivateSonar : MonoBehaviour
 {
-    public GameObject targetGameObject;
+    public GameObject sonar;
     private Tween scaleTween; //tween型の変数scaletween
     
 
     void Update()
     {
-        // 左Shiftか右Shiftのいずれかが押されたとき
+        // 左Shiftか右Shiftのいずれかが押されたとき <-「押下し時」じゃなくて「押してる最中」にする
         if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
         {
-            if (targetGameObject != null)
-            {
-                targetGameObject.SetActive(true); // ゲームオブジェクトをアクティブにする
-                scaleTween = targetGameObject.transform.DOScale(new Vector3(2f, 2f, 2f), 3f); // スケールを0から2にアニメーションする
-            }
+            // キーを押してる時にソナーの大きさが最大じゃなかったら
+                // ソナーを最大まで大きくする
+
+            // 
+            //if (sonar != null)
+            //{
+            sonar.SetActive(true); // ゲームオブジェクトをアクティブにする
+                scaleTween = sonar.transform.DOScale(new Vector3(2f, 2f, 2f), 1.5f).SetLoops(1, LoopType.Yoyo); ; // スケールを0から2にアニメーションする
+               
+            // sonar.transform.DOScale(Vector3.zero, 2f);
+            //}
         }
 
         // 左Shiftか右Shiftのいずれかを離したとき
         if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
         {
-            if (targetGameObject != null)
-            {
+            // キーを離した時にソナーの大きさが0じゃなかったら
+                // ソナーを0まで小さくする
+            //if (sonar != null)
+            //{
+            /*
                 if (scaleTween != null && scaleTween.IsActive())
                 {
                     scaleTween.Kill(); // 現在のスケールアニメーションを停止
@@ -32,10 +41,11 @@ public class ActivateSonar : MonoBehaviour
                 // スケールを徐々に0にアニメーションする
                 // DOScale(Vector3.zero, 2f) 2秒後に元の大きさに戻す。
                 // OnComplete コールバック関数　ある関数が実行された後に呼び出される関数を無視する
-                targetGameObject.transform.DOScale(Vector3.zero, 2f).OnComplete(() => {
-                    targetGameObject.SetActive(false); // アニメーション完了後、ゲームオブジェクトを非アクティブにする
+                sonar.transform.DOScale(Vector3.zero, 2f).OnComplete(() => {
+                    sonar.SetActive(false); // アニメーション完了後、ゲームオブジェクトを非アクティブにする
                 });
-            }
+            */
+            //}
         }
     }
 }
