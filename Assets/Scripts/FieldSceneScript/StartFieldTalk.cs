8bf8ea0d272349f960ee0602db916c9c111fb594
yuuka83 <s2210017@jaist.ac.jp>
2024-01-21 03:53:52 +0900

    StartFieldTalkでTalkパネルが一回しか出ないように修正

--- Assets/Scripts/FieldSceneScript/StartFieldTalk.cs
+++ Assets/Scripts/FieldSceneScript/StartFieldTalk.cs
@@ -9,11 +9,19 @@ public class StartFieldTalk : MonoBehaviour
     [SerializeField] string message;
     [SerializeField] string imagePath;
 
+    bool isTalked;// 一度表示されたかどうか
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
+            if(isTalked == true)
+            {
+                Destroy(this.gameObject);
+                return;
+            }
             fieldTalk.DisplayFieldText(message, imagePath);
+            isTalked = true;
         }
     }
 }
