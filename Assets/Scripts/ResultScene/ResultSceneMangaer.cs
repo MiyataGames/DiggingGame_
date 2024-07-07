e762a2c198c298219b6999c6ed46b69cab502888
yuuka83 <s2210017@jaist.ac.jp>
2024-07-07 21:10:24 +0900

    ２個目以降の経験値が出てこない問題を修正

--- Assets/Scripts/ResultScene/ResultSceneMangaer.cs
+++ Assets/Scripts/ResultScene/ResultSceneMangaer.cs
@@ -15,23 +15,24 @@ public class ResultSceneMangaer : MonoBehaviour
 {
     private ResultState resultState = ResultState.RESULT_LEVELUP_ANIM;
     [SerializeField] private Transform winPlayerPos;
+
     //[SerializeField] private Camera resultSceneCamera;
     [SerializeField] private GameObject resultPanel;
-    [SerializeField] GameObject resultAreaPanel;
-    [SerializeField] GameObject resultDropPanel;
+
+    [SerializeField] private GameObject resultAreaPanel;
+    [SerializeField] private GameObject resultDropPanel;
     [SerializeField] private GameObject dropItemAreaPanel;
-    [SerializeField] GameObject dropItemTextPrefab;
-    [SerializeField] GameObject dropGoldText;
+    [SerializeField] private GameObject dropItemTextPrefab;
+    [SerializeField] private GameObject dropGoldText;
     private static readonly int hashWin = Animator.StringToHash("Base Layer.Win");
     [SerializeField] private GameManager gameManager;
     private GameObject playerModel;
 
-
     private void Update()
     {
-        if(resultState == ResultState.RESULT_LEVELUP_END)
+        if (resultState == ResultState.RESULT_LEVELUP_END)
         {
-            if(Input.GetKeyDown(KeyCode.Return))
+            if (Input.GetKeyDown(KeyCode.Return))
             {
                 resultState = ResultState.RESULT_DROP;
                 // 経験値リザルトをオフ
@@ -41,17 +42,19 @@ public class ResultSceneMangaer : MonoBehaviour
                 // 必要な情報を書き込む
                 ShowDropObjectsText();
             }
-        }else if(resultState == ResultState.RESULT_DROP)
+        }
+        else if (resultState == ResultState.RESULT_DROP)
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 DeleteDropObjectTexts();
                 gameManager.CurrentSceneIndex = (int)GameMode.FIELD_SCENE;
                 // リザルトの子オブジェクトを削除
-                Destroy(playerModel);
-                resultPanel.SetActive(false);
+                //Destroy(playerModel);
+                //resultPanel.SetActive(false);
+                // 経験値リザルトをオン
+                resultAreaPanel.SetActive(true);
             }
-
         }
         /*
         if (resultState == ResultState.END)
@@ -69,7 +72,6 @@ public class ResultSceneMangaer : MonoBehaviour
     public IEnumerator ResultPlayer(Player player)
     {
         // リザルトをオン
-        
         //playerModel = Instantiate(player.PlayerBase.PlayerModel, winPlayerPos.position, Quaternion.identity);
         // Result????????????
         resultState = ResultState.RESULT_LEVELUP_ANIM;
@@ -83,30 +85,32 @@ public class ResultSceneMangaer : MonoBehaviour
         resultState = ResultState.RESULT_LEVELUP_END;
     }
 
-    void ShowDropObjectsText()
+    private void ShowDropObjectsText()
     {
+        // ドロップアイテムを表示
+        resultDropPanel.SetActive(true);
         DropObjectsStruct dropObjects = GameManager.instance.DropObjects;
         // テキストを表示
         dropGoldText.GetComponent<TextMeshProUGUI>().text = dropObjects.totalDropGold.ToString();
-        for(int i = 0; i< dropObjects.dropItems.Count; i++)
+        for (int i = 0; i < dropObjects.dropItems.Count; i++)
         {
-            TextMeshProUGUI text =  Instantiate(dropItemTextPrefab, dropItemAreaPanel.transform).GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI text = Instantiate(dropItemTextPrefab, dropItemAreaPanel.transform).GetComponent<TextMeshProUGUI>();
             text.text = dropObjects.dropItems[i].ItemBase.ItemName.ToString();
         }
     }
 
-    void DeleteDropObjectTexts()
+    private void DeleteDropObjectTexts()
     {
-
         // 子オブジェクトの数を取得
         int childCount = dropItemAreaPanel.transform.childCount;
-
         // 子オブジェクトを順に取得する
         for (int i = 0; i < childCount; i++)
         {
-            Transform childTransform = dropItemAreaPanel.transform.GetChild(0);
+            Transform childTransform = dropItemAreaPanel.transform.GetChild(i);
             GameObject childObject = childTransform.gameObject;
             Destroy(childObject);
         }
+        // ドロップアイテムを非表示
+        resultDropPanel.SetActive(false);
     }
 }
